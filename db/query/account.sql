8eddfeba1b3a5394db7f4ade84ef024e124066ca
ramesh <thebuff27@mail.ru>
2023-07-22 01:09:59 +0700

    добавил получение аккаунтов из бд

--- db/query/account.sql
+++ db/query/account.sql
@@ -1,2 +1,8 @@
 -- name: CreateAccount :one
 insert into accounts (owner, balance, currency) values ($1, $2, $3) returning *;
+
+-- name: GetAccount :one
+select * from accounts where id = $1 limit 1;
+
+-- name: ListAccounts :many
+select * from accounts order by id limit $1 offset $2;
